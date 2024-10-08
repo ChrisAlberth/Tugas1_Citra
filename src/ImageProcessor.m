fc15d6446baa7fa163049e100ecf2f7f39b3164d
Chris Albert <christiangultom5@gmail.com>
2024-10-08 23:28:19 +0700

    chore: comment function

--- src/ImageProcessor.m
+++ src/ImageProcessor.m
@@ -1,26 +1,28 @@
 classdef ImageProcessor
     methods
         function histogram = calculateGrayscaledHistogram(~, image)
-            [row, col] = size(image);
-            histogram = zeros(1, 256);
-    
+            [row, col] = size(image); 
+            histogram = zeros(1, 256); % Inisialisasi histogram 
+            
+            % Loop setiap pixel untuk mendapat intensity
             for i = 1:row
                 for j = 1:col
                     intensity = image(i, j);
-                    histogram(intensity + 1) = histogram(intensity + 1) + 1;
+                    histogram(intensity + 1) = histogram(intensity + 1) + 1; % Tambah pada intensity yang sesuai
                 end
             end
         end
-
+        
         function histogram = calculateColoredHistogram(~, image)
             [row, col, channel] = size(image);
-            histogram = zeros(3, 256);
-    
+            histogram = zeros(3, 256); % Inisialisasi 3 histogram untuk tiap warna
+            
+            % Loop setiap pixel pada tiap channel warna untuk mendapat intensity
             for c = 1:channel
                 for i = 1:row
                     for j = 1:col
                         intensity = image(i, j, c); 
-                        histogram(c, intensity + 1) = histogram(c, intensity + 1) + 1;
+                        histogram(c, intensity + 1) = histogram(c, intensity + 1) + 1; % Tambah pada intensity yang sesuai
                     end
                 end
             end
@@ -31,39 +33,41 @@ classdef ImageProcessor
         end
 
         function result = imageNegative(~, image)
-            result = 255 - image;
+            result = 255 - image; % Membalikan intensitas piksel
         end
 
         function result = logTransformation(~, image, c)
-            result = im2double(image);
-            result = c * log(result + 1);
-            result = im2uint8(result);
+            result = im2double(image); % Mengubah menjadi double
+            result = c * log(result + 1); % Melakukan transformasi log
+            result = im2uint8(result); % Mengembalikan ke uint8
         end
 
         function result = powerTransformation(~, image, c, gamma)
-            result = im2double(image);
-            result = c * (result.^gamma);
-            result = im2uint8(result);
+            result = im2double(image); % Mengubah menjadi double
+            result = c * (result.^gamma); % Melakukan transformasi pangkat
+            result = im2uint8(result); % Mengembalikan ke uint8
         end
 
         function result = contrastStretching(~, image)
-            rmin = min(image(:));
-            rmax = max(image(:));
-            result = (image - rmin) .* (255 / (rmax - rmin));
+            rmin = min(image(:)); % Mendapat intensitas minimal citra
+            rmax = max(image(:)); % Mendapat intensitas maksimal citra
+            result = (image - rmin) .* (255 / (rmax - rmin)); % Melakukan stretching
         end
 
         function result = histogramEqualization(self, image)
-            imageHistogram = self.calculateGrayscaledHistogram(image);
+            imageHistogram = self.calculateGrayscaledHistogram(image); % Memperoleh histogram citra
             [row, col] = size(image);
             n = row * col;
-            eqHistogram = zeros(1, 256);
-
+            eqHistogram = zeros(1, 256); % Inisialisasi equalizer histogram
+            
+            % Mengisi equalizer histogram
             sum = 0;
             for i = 1:256
                 sum = sum + imageHistogram(i);
                 eqHistogram(i) = floor((double(sum) / n) * 255);
             end
-
+            
+            % Update citra sesuai dengan equalizer histogram
             result = image;
             for i = 1:row
                 for j = 1:col
@@ -75,28 +79,30 @@ classdef ImageProcessor
         function result = histogramSpecification(self, image, ref)
             [row, col] = size(image);
             n = row * col;
-
-            imageHistogram = self.calculateGrayscaledHistogram(image);
-            imageEqHist = zeros(1, 256);
-
+            
+            imageHistogram = self.calculateGrayscaledHistogram(image); % Hitung histogram citra
+            imageEqHist = zeros(1, 256); % Inisialisasi eqHist citra
+            
+            % Mengisi eqHist citra
             sum = 0;
             for i = 1:256
                 sum = sum + imageHistogram(i);
                 imageEqHist(i) = floor((double(sum) / n) * 255);
             end
 
-            refHistogram = self.calculateGrayscaledHistogram(ref);
-            refEqHist = zeros(1, 256);
-
+            refHistogram = self.calculateGrayscaledHistogram(ref); % Hitung histogram referensi
+            refEqHist = zeros(1, 256); % Inisialisasi eqHist referensi
+            
+            % Mengisi eqHist referensi
             sum = 0;
             for i = 1:256
                 sum = sum + refHistogram(i);
                 refEqHist(i) = floor((double(sum) / n) * 255);
             end
-    
+            
+            % Transformasi balikan
             invHist = zeros(1, 256);
             for i = 1:row
-                disp(max(i/(row/256), 1));
                 minVal = abs(imageEqHist(uint8(max(i/(row/256), 1))) - refEqHist(1));
                 minJ = 0;
 
@@ -109,7 +115,8 @@ classdef ImageProcessor
 
                 invHist(i) = minJ;
             end
-    
+            
+            % Update citra
             result = image;
             for i = 1:row
                 for j = 1:col
